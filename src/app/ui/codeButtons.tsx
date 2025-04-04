9b229c408d85077ac0c927634d0f7a8acfdfb7f4
Guilherme Guimarães Cazaroto <gui.cazaroto@gmail.com>
2025-04-04 20:26:58 -0300

    save

--- src/app/ui/codeButtons.tsx
+++ src/app/ui/codeButtons.tsx
@@ -1,12 +1,23 @@
+'use client';
+import { useState } from "react";
+
 type CodeButtonsProps = { 
   languages: Array<{ name: string; ext: string }> 
 }
 
 export default function CodeButtons({ languages }: CodeButtonsProps) {  
+  const [selectedLanguage, setSelectedLanguage] = useState(languages[0].name);
+
   return (
     <div className="flex gap-3">
-      {languages.map((language) => (
-        <button key={language.name} className={`code-button code-button--active ${language.name}`}>
+      {languages.map((language, i) => (
+        <button 
+          key={language.name} 
+          className={
+          `code-button ${language.name} ${language.name === selectedLanguage ? 'code-button--active' : ''}`
+          }
+          
+        >
           <img className="h-[53px] w-[53px]" src={`/img/${language.name}.svg`} alt="Code" />
         </button>
       ))}
