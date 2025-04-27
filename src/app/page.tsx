941fdf5c39e644dd155dc6ac86a4bf0a236ac844
Guilherme Guimarães Cazaroto <gui.cazaroto@gmail.com>
2025-04-27 19:09:46 -0300

    feat: add home page illustrations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -23,6 +24,59 @@ export default function Home() {
       <p>
         This project is inspired by <a href="https://refactoring.guru/" target="_blank" className="link">Refactoring.Guru</a>, which does an amazing job of explaining refactoring, design patterns, and SOLID principles in a clear and connected way. I wanted to bring that same clarity and playfulness to another essential area of programming: algorithms and data structures.
       </p>
+
+      <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+
+      <div className="flex flex-row mt-6 space-between">
+        <Image 
+          src="/img/algorithms.webp" 
+          alt="Algorithms" 
+          width={498} 
+          height={493} 
+        />
+
+        <div className="ml-6 flex flex-col justify-between">
+          <div>
+            <h2 className="font-bold text-2xl text-gray-700 mt-4">
+              Algorithms
+            </h2>
+            <p>
+              Algorithms are the heart of programming. They’re the step-by-step instructions that tell your computer how to solve a problem or perform a task.
+            </p>
+          </div>
+
+          <Link href="/algorithms" className="btn-info w-fit">
+            More about Algorithms
+            <i className="ml-2 fa fa-arrow-right" aria-hidden="true"></i>
+          </Link>
+        </div>
+      </div>
+
+      <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+
+      <div className="flex flex-row mt-6 space-between">
+        <Image 
+          src="/img/data-structures.webp" 
+          alt="Algorithms" 
+          width={498} 
+          height={493} 
+        />
+        <div className="ml-6 flex flex-col justify-between">
+          <div>
+            <h2 className="font-bold text-2xl text-gray-700 mt-4">
+              Data Structures
+            </h2>
+            <p>
+              Data structures are the way we organize and store data in our programs. They’re like the containers that hold the information we need to work with.
+            </p>
+          </div>
+
+          <Link href="/data-structures" className="btn-info w-fit">
+            More about Data Structures
+            <i className="ml-2 fa fa-arrow-right" aria-hidden="true"></i>
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
